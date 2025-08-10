9dea7cb5ef0d961d07fe884a15a06edec2a5dd3b
S M Shawon <shawon2jg@yahoo.com>
2025-08-10 12:15:49 +1200

    Week_02

--- activityFive.py
+++ activityFive.py
@@ -1,13 +1,13 @@
-class Sentence:
+class SentenceCheck:
     def __init__(self, text):
         self.text = text
 
     def get_words_number(self):
-        words = self.text.split()
+        words = self.text.strip().split()
         return len(words)
 
 if __name__ == "__main__":
-    sentence = input("Please input a sentence: ")
-    word = Sentence(sentence)
-    word_count = word.get_words_number()
+    sentence = input("Enter a sentence: ")
+    object = SentenceCheck(sentence)
+    word_count = object.get_words_number()
     print(f"The Number of words in the sentence: {word_count}")
