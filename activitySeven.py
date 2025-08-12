9fb0bbe36912c76e666bf6b9babef69d66028487
S M Shawon <shawon2jg@yahoo.com>
2025-08-12 19:48:49 +1200

    Week_02

--- activitySeven.py
+++ activitySeven.py
@@ -1,3 +1,11 @@
+#region Activity 07
+# You are tasked with developing a simple program for the Human Resources (HR) department to store and display basic employee information, including each employee’s name, salary, and job title.
+# Requirements:
+# 1.Create at least two Employee objects with different data.
+# 2.Call the display_info() method to show each employee’s details.
+# 3.Call the give_raise() method to increase an employee’s salary and display the updated amount.
+#endregion
+
 class Employee:
     def __init__(self):
         self.employee_list = []
@@ -9,14 +17,28 @@ class Employee:
             self.employee_list.append(input("Enter the employee's salary: "))
             self.employee_list.append(input("Enter the employee's job tile: "))
 
-    def display_details(self):
+    def display_info(self):
+        count = 2
         print("All Employee Information: ")
         for index, item in enumerate(self.employee_list):
             print(item, end=" ")
-            if index == 2:
+            if index == count:
+                count += 3
                 print("")
+
+    def give_raise(self):
+        count = 1
+        increment = int(input(f"\nEnter the percentage increase for an employee's salary: "))
+        for index, item in enumerate(self.employee_list):
+            if index == count:
+                self.employee_list[index] = int(self.employee_list[index]) + (increment / 100)
+                count += 3
 
 if __name__ == "__main__":
     objPerson = Employee()
+
     objPerson.insert_employee_details()
-    objPerson.display_details()
+    objPerson.display_info()
+
+    objPerson.give_raise()
+    objPerson.display_info()
