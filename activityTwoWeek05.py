a8b5bb74bcf4155cf72b3270250e67d11440c563
S M Shawon <shawon2jg@yahoo.com>
2025-08-30 09:29:25 +1200

    inheritance in python with example

---
+++ activityTwoWeek05.py
@@ -0,0 +1,19 @@
+class Person:
+    # Represents a general person
+    def __init__(self, id, name, address, age):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.age = age
+
+class Teacher(Person):
+    def __init__(self, id, name, address, age, tax_code, salary):
+        super().__init__(id, name, address, age)  # Call the parent class's constructor
+        self.tax_code = tax_code
+        self.salary = salary
+
+class Staff(Person):
+    def __init__(self, id, name, address, age, tax_code, pay_rate):
+        super().__init__(id, name, address, age)  # Call the parent class's constructor
+        self.tax_code = tax_code
+        self.pay_rate = pay_rate
