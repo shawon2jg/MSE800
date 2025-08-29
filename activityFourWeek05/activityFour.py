26842b391a4a89724593f738856740aadbda3376
S M Shawon <shawon2jg@yahoo.com>
2025-08-30 10:15:58 +1200

    overriding in python with example

--- activityFourWeek05/activityFour.py
+++ activityFourWeek05/activityFour.py
@@ -2,8 +2,8 @@ from MSE800.activityFourWeek05.Person import Person
 
 class Student(Person):
     def __init__(self, name, address, age, student_id):
-        super().__init__(name, address, age)  # Call the parent class's constructor
+        super().__init__(name, address, age)
         self.student_id = student_id
 
 if __name__ == '__main__':
-    objStudent = Student("Kamal Bin Shafiq ", "Avondale", 20, "S001")
+    objStudent = Student("S M Shawon", "Avondale", 35, "270702380")
