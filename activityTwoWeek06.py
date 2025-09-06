6165bd1c270898f4092d907911f56aba0d00ec61
S M Shawon <shawon2jg@yahoo.com>
2025-09-07 11:41:26 +1200

    sort the dictionary based on the ages using lambda

--- activityTwoWeek06.py
+++ activityTwoWeek06.py
@@ -1,6 +1,22 @@
-
+# region Part One
 data = ['a5', 'a2', 'b1', 'b3', 'c2']
 
 # Lambda Key: For multi-level sorting (letter first then number)
 sorted_data = sorted(data, key=lambda x: (x[0], int(x[1:])))
 print(sorted_data)
+# endregion
+
+# region Part Two
+# sort the dictionary based on the ages using lambda
+students = [
+    {'name': "John", 'grade': "A", 'age': 20},
+    {'name': "Jane", 'grade': "B", 'age': 21},
+    {'name': "Joss", 'grade': "A+", 'age': 19},
+    {'name': "Jack", 'grade': "A-", 'age': 16},
+    {'name': "Dave", 'grade': "C", 'age': 25},
+]
+
+# Sort the list using lambda to access the 'age' key
+sorted_students = sorted(students, key=lambda x: x['age'], reverse=False)
+print(sorted_students)
+# endregion
