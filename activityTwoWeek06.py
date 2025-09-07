e7d2cf3d9ae19a43d4d183b09bc532b5fa20b9f9
S M Shawon <shawon2jg@yahoo.com>
2025-09-07 12:10:59 +1200

    using a Lambda function

--- activityTwoWeek06.py
+++ activityTwoWeek06.py
@@ -22,18 +22,31 @@ data = ['a5', 'a2', 'b1', 'b3', 'c2']
 # endregion
 
 # region Part Three
-students = [
-    {'name': "John", 'grade': "A", 'age': 20},
-    {'name': "Jane", 'grade': "B", 'age': 21},
-    {'name': "Joss", 'grade': "A+", 'age': 19},
-    {'name': "Jack", 'grade': "A-", 'age': 16},
-    {'name': "Dave", 'grade': "C", 'age': 25},
-]
-
-sorted_students = sorted(students, key=lambda x: (x['grade'] != "A+", x['grade'], x['age']), reverse=False)
-print(sorted_students)
+# students = [
+#     {'name': "John", 'grade': "A", 'age': 20},
+#     {'name': "Jane", 'grade': "B", 'age': 21},
+#     {'name': "Joss", 'grade': "A+", 'age': 19},
+#     {'name': "Jack", 'grade': "A-", 'age': 16},
+#     {'name': "Dave", 'grade': "C", 'age': 25},
+# ]
+#
+# sorted_students = sorted(students, key=lambda x: (x['grade'] != "A+", x['grade'], x['age']), reverse=False)
+# print(sorted_students)
 # endregion
 
 # region Part Four
+employees = [
+    {'name': 'Alice', 'age': 30, 'salary': 80000},
+    {'name': 'Bob', 'age': 25, 'salary': 50000},
+    {'name': 'Charlie', 'age': 35, 'salary': 120000},
+]
 
+def sort_key(x):
+    return (x['age'], x['salary'])
+
+with_lambda = sorted(employees, key=lambda x: (x['age'], x['salary']))
+without_lambda = sorted(employees, key=sort_key)
+
+print(f"With using the Lambda Function\n: {with_lambda}")
+print(f"Without using the Lambda Function\n: {without_lambda}")
 # endregion
