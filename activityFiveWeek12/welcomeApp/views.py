c3c63d17582ba1463a383a46e412065ce577b47f
S M Shawon <shawon2jg@yahoo.com>
2025-10-19 11:24:41 +1300

    Develop a web application with Django

---
+++ activityFiveWeek12/welcomeApp/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def welcome(request, name):
+    """
+    A simple Django view that takes a name from the URL
+    and returns a personalized welcome message.
+    """
+    return HttpResponse(f"<h1>Welcome {name} to Django!</h1>")
